d8e22f76995783b570b09aedd6526a20b8e9f783
kevinzhao2233 <kevin_2233@qq.com>
2021-03-01 17:36:17 +0800

    📦 添加页面底部布局组件

---
+++ src/layout/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+
+const Footer: React.FC = () => (
+  <div className="page-footer">
+    <p>Copyright © 2021 kevinzhao2233</p>
+    <p>京备 123456789</p>
+  </div>
+);
+
+export default Footer;
